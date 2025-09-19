f4af51dece05a167bcbb2d34eca71c58e916c374
Yomari <yassinomari75@gmail.com>
2025-09-19 07:54:54 +0100

    starting the project!

---
+++ backend/src/main/java/myapp/HomeController.java
@@ -0,0 +1,12 @@
+package myapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello Web!";
+	}
+}
